4e1acb77124dffe4c2940ce0e2b3e399ea5e7d5e
Shreyash2003 <104262663+Shreyash2003@users.noreply.github.com>
2022-05-06 21:53:48 +0530

    Add files via upload

---
+++ assignment output.py
@@ -0,0 +1,36 @@
+C:\Users\shrey\PycharmProjects\pythonProject25\venv\Scripts\python.exe C:/Users/shrey/PycharmProjects/pythonProject25/main.py
+A) Length of the given string:  35
+
+B) Reverse of the given string is :
+egaugnal evitisnes esac a si nohtyP
+
+C) New sliced string is:
+ a case sensitive
+
+D) String after replacing:
+ Python is object oriented language
+
+E) Index of substring 'a' is: 10
+
+F) Sting without white spaces:
+ Pythonisacasesensitivelanguage
+Hey, Shreyash Raj Here!
+My SID is 21107106
+I am from Mechanical Engineering department and my CGPA is 9.9
+A. a & b = 8
+B. a | b = 58
+C. a ^ b = 50
+D. a << 2 =  224
+E. a >> 2 =  14  and  b >> 4 =  0
+Enter any string: My name is Shreyash Raj
+Yes
+Enter First length : 16
+Enter Second length : 9
+Enter Third length : 25
+The triangle can be formed?
+Yes!
+Enter 1st number (a) 3
+Enter 2nd number (b) 4
+Number of bits needed to be flipped to convert ‘a’ to ‘b’ are: 3
+
+Process finished with exit code 0
